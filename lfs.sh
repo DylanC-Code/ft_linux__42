aef3b211f734308c20309bac3b0b6a427c4b5f83
dylanc_code <dylanc.code@gmail.com>
2025-11-01 22:34:41 +0100

    feat: initial scripts for LFS setup and disk partitioning added

---
+++ lfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LFS=/mnt/LFS
+export LFS_TGT=x86_64-lfs-linux-gnu
+export LFS_DISK=/dev/sda
+
+
+if ! grep -q "$LFS" /proc/mounts; then
+    source setupdisk.sh "$LFS_DISK"
+fi
