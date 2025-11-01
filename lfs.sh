c2f802661b8a5abed666a856917b942bef067b7d
dylanc_code <dylanc.code@gmail.com>
2025-11-01 23:09:55 +0100

    fix: correct LFS directory path and ensure necessary directories are created

--- lfs.sh
+++ lfs.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-export LFS=/mnt/LFS
+export LFS=/mnt/lfs
 export LFS_TGT=x86_64-lfs-linux-gnu
 export LFS_DISK=/dev/sda
 
+if [ ! -d "$LFS" ]; then
+    sudo mkdir -vp "$LFS"
+fi
 
 if ! grep -q "$LFS" /proc/mounts; then
     source setupdisk.sh "$LFS_DISK"
+    sudo mount "${LFS_DISK}4" "$LFS"
+    sudo chown -v $USER "$LFS"
 fi
+
+mkdir -pv $LFS/sources
+mkdir -pv $LFS/tools
+
+mkdir -pv $LFS/boot
+mkdir -pv $LFS/etc
+mkdir -pv $LFS/bin
+mkdir -pv $LFS/lib
+mkdir -pv $LFS/sbin
+mkdir -pv $LFS/usr
+mkdir -pv $LFS/var
+
+case $(uname -m) in
+    x86_64) mkdir -pv $LFS/lib64 ;;
+esac
