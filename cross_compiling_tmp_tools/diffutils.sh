8e9a2295179c5d27702ab1391c325fb606672d70
dylanc_code <dylanc.code@gmail.com>
2025-11-02 17:08:16 +0100

    feat: add diffutils build script for cross-compiling tools

---
+++ cross_compiling_tmp_tools/diffutils.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            gl_cv_func_strcasecmp_works=y \
+            --build=$(./build-aux/config.guess) \
+&& make \
+&& make DESTDIR=$LFS install
