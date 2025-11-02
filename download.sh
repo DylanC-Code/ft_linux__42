5f6633a9382a8c46eb43d1168b23c5e04583505a
dylanc_code <dylanc.code@gmail.com>
2025-11-02 14:52:26 +0100

    feat: enhance download script with clearer output messages for package and patch downloads

--- download.sh
+++ download.sh
@@ -9,7 +9,28 @@ cat packages.csv | while read line; do
 
     if [ ! -f "$CACHEFILE" ]; then
 
-        echo "Downloading $URL"
+        echo "Downloading package: $URL"
+        wget "$URL"
+
+        if ! echo "$MD5SUM $CACHEFILE" | md5sum -c > /dev/null; then
+            rm -f "$CACHEFILE"
+            echo "Verification of $CACHEFILE failed! MD5 mismatch!"
+            exit 1
+        fi 
+
+    fi
+done
+
+cat patches.csv | while read line; do
+    NAME="`echo  $line | cut -d\; -f1`"
+    VERSION="`echo  $line | cut -d\; -f2`"
+    URL="`echo  $line | cut -d\; -f3 | sed "s/@/$VERSION/g"`"
+    MD5SUM="`echo  $line | cut -d\; -f4`"
+    CACHEFILE="$(basename "$URL")"
+
+    if [ ! -f "$CACHEFILE" ]; then
+
+        echo "Downloading patch: $URL"
         wget "$URL"
 
         if ! echo "$MD5SUM $CACHEFILE" | md5sum -c > /dev/null; then
