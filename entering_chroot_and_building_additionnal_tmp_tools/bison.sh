30bb57cacdd4a0f30a37cb21cf3bad4fbd2dd9d7
dylanc_code <dylanc.code@gmail.com>
2025-11-05 14:05:51 +0100

    feat: add bison and perl build scripts; enhance preparechroot.sh with mount checks

---
+++ entering_chroot_and_building_additionnal_tmp_tools/bison.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./configure --prefix=/usr \
+            "--docdir=/usr/share/doc/bison-$VERSION"
+
+make
+make install
--- entering_chroot_and_building_additionnal_tmp_tools/preparechroot.sh
+++ entering_chroot_and_building_additionnal_tmp_tools/preparechroot.sh
@@ -15,16 +15,46 @@ esac
 
 mkdir -pv $LFS/{dev,proc,sys,run}
 
-mount -v --bind /dev $LFS/dev
+# --- /dev ---
+if ! mountpoint -q $LFS/dev; then
+    mount -v --bind /dev $LFS/dev
+else
+    echo "$LFS/dev already mounted"
+fi
 
-mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
+# --- /dev/pts ---
+if ! mountpoint -q $LFS/dev/pts; then
+    mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
+else
+    echo "$LFS/dev/pts already mounted"
+fi
 
+# --- /proc ---
+if ! mountpoint -q $LFS/proc; then
+    mount -vt proc proc $LFS/proc
+else
+    echo "$LFS/proc already mounted"
+fi
 
+# --- /sys ---
+if ! mountpoint -q $LFS/sys; then
+    mount -vt sysfs sysfs $LFS/sys
+else
+    echo "$LFS/sys already mounted"
+fi
+
+# --- /run ---
+if ! mountpoint -q $LFS/run; then
+    mount -vt tmpfs tmpfs $LFS/run
+else
+    echo "$LFS/run already mounted"
+fi
+
+# --- /dev/shm ---
 if [ -h $LFS/dev/shm ]; then
     install -v -d -m 1777 $LFS$(realpath /dev/shm)
-else
+elif ! mountpoint -q $LFS/dev/shm; then
     mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
-fi  
+else
+    echo "$LFS/dev/shm already mounted"
+fi
